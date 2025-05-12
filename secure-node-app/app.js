7cfdbe4ce3e1d5ba8d48961f0bdfa19c96c8a95a
prasannatdl <144869278+prasannatdl@users.noreply.github.com>
2025-05-12 12:56:03 +0530

    Update app.js

--- secure-node-app/app.js
+++ secure-node-app/app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = 3000;
+const port = 3001;
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Secure Docker App\n');
